80c373fc17381403b4338f7e1b1cb9a70529ca6b
Disney <disneyconstant996@gmail.com>
2024-08-03 20:54:08 +0300

    Add styling and interactivity

---
+++ src/students.js
@@ -0,0 +1,16 @@
+export const studentList = [
+    {
+        firstName: 'Tanya',
+        lastName: 'Sinclair',
+        profession: 'UX Engineer',
+        testimony: "I've been interested in coding for a while but never taken the jump, until now. I couldn't recommend this course enough. I'm now in the job of my dreams and so excited about the future.",
+        cover: '/src/assets/images/image-tanya.jpg'
+    },
+    {
+        firstName: 'John',
+        lastName: 'Tarkpor',
+        profession: 'Junior Front-end Developer',
+        testimony: "If you want to lay the best foundation possible I'd recommend taking this course. The depth the instructors go into is incredible. I now feel so confindent about starting up as a professional developer.",
+        cover: "/src/assets/images/image-john.jpg"
+    }
+]
